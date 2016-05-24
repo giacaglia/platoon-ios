23f1743884982a5036ada9c14261bbb9c0807960
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-24 11:18:29 -0400

    Add Carthage to the folder and add Cartography via Carthage

---
+++ platoon-ios/GetStartedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GetStartedViewController.swift
+//  platoon-ios
+//
+//  Created by Giuliano Giacaglia on 5/24/16.
+//  Copyright © 2016 Platoon. All rights reserved.
+//
+
+import UIKit
+import Cartography
+
+class GetStartedViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
