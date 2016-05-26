1330b132b4e318551a6db724ecd7fce063ec449d
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-25 23:25:57 -0400

    Add map to the load vc

--- platoon-ios/LoadViewController.swift
+++ platoon-ios/LoadViewController.swift
@@ -13,22 +13,36 @@ import Cartography
 class LoadViewController : UIViewController {
     
     let mapView = MKMapView()
+    let topMapView = MKMapView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .whiteColor()
-        
         
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
         self.title = "Greylock Importers"
         
         self.view.addSubview(mapView)
-        self.addMap()
+        self.addMap(mapView)
         
         constrain(mapView) { mapView in
             mapView.top    == mapView.superview!.top + 64
             mapView.left   == mapView.superview!.left
             mapView.right  == mapView.superview!.right
-            mapView.height == 250
+            mapView.height == 210
+        }
+        
+        self.view.addSubview(topMapView)
+        self.addMap(topMapView)
+        
+        
+        topMapView.layer.cornerRadius = 92
+        topMapView.layer.borderColor = AppearanceManager.sharedInstance.cerulean.CGColor
+        topMapView.layer.borderWidth = 4
+        constrain(topMapView) { topMapView in
+            topMapView.top     == topMapView.superview!.top + 72
+            topMapView.centerX == topMapView.superview!.centerX
+            topMapView.width  == 184
+            topMapView.height == 184
         }
     }
     
@@ -40,6 +54,7 @@ class LoadViewController : UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.addBlur()
+        self.view.bringSubviewToFront(topMapView)
     }
     
     private func addBlur() {
@@ -50,7 +65,7 @@ class LoadViewController : UIViewController {
         self.view.addSubview(blurEffectView)
     }
     
-    private func addMap() {
+    private func addMap(mapView:MKMapView) {
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         let regionRadius: CLLocationDistance = 1000
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(initialLocation.coordinate,
