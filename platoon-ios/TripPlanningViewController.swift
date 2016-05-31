850817cab23dac45c98742d887ac15b49a4314ee
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-31 12:04:03 -0400

    Add month to the trip planning part of the app

--- platoon-ios/TripPlanningViewController.swift
+++ platoon-ios/TripPlanningViewController.swift
@@ -13,9 +13,51 @@ class TripPlanningViewController: UIViewController {
     
     let tableView = UITableView()
     let dateCollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
+    let monthView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .whiteColor()
+        self.title = "Trip Planning"
+        self.view.backgroundColor = AppearanceManager.sharedInstance.backgroundColor
+        
+        
+        monthView.backgroundColor = .whiteColor()
+        self.view.addSubview(monthView)
+        
+        let monthLabel = UILabel()
+        monthLabel.text = "November"
+        monthLabel.textAlignment = .Center
+        monthLabel.font = AppearanceManager.lightFont(14)
+        monthLabel.textColor = .blackColor()
+        monthView.addSubview(monthLabel)
+        
+        let leftLine = UIView()
+        leftLine.backgroundColor = UIColor(white: 151/255.0, alpha: 1.0)
+        monthView.addSubview(leftLine)
+
+        let rightLine = UIView()
+        rightLine.backgroundColor = UIColor(white: 151/255.0, alpha: 1.0)
+        monthView.addSubview(rightLine)
+        
+        constrain(monthView, monthLabel, leftLine, rightLine) { monthView, monthLabel, leftLine, rightLine in
+            monthView.top    == monthView.superview!.top + 64
+            monthView.left   == monthView.superview!.left
+            monthView.right  == monthView.superview!.right
+            monthView.height == 26
+            
+            monthLabel.center == monthLabel.superview!.center
+            
+            leftLine.left    == leftLine.superview!.left
+            leftLine.right   == monthLabel.left - 16
+            leftLine.centerY == leftLine.superview!.centerY
+            leftLine.height  == 1
+            
+            rightLine.left    == monthLabel.right + 16
+            rightLine.right   == monthLabel.superview!.right
+            rightLine.centerY == rightLine.superview!.centerY
+            rightLine.height  == 1
+        }
+        
         
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.scrollDirection = .Horizontal
@@ -27,13 +69,21 @@ class TripPlanningViewController: UIViewController {
         dateCollectionView.backgroundColor = .whiteColor()
         dateCollectionView.showsVerticalScrollIndicator = false
         dateCollectionView.showsHorizontalScrollIndicator = false
+        
+        dateCollectionView.layer.shadowOpacity = 0.19
+        dateCollectionView.layer.shadowColor = UIColor.blackColor().CGColor
+        dateCollectionView.layer.shadowOffset = CGSizeMake(0, 4)
+        dateCollectionView.layer.shadowRadius = 10
+        dateCollectionView.layer.masksToBounds = false
         self.view.addSubview(dateCollectionView)
         constrain(dateCollectionView) { dateCollectionView in
-            dateCollectionView.top    == dateCollectionView.superview!.top + 64
+            dateCollectionView.top    == dateCollectionView.superview!.top + 90
             dateCollectionView.left   == dateCollectionView.superview!.left
             dateCollectionView.right  == dateCollectionView.superview!.right
             dateCollectionView.height == 70
         }
+        
+        self.view.bringSubviewToFront(monthView)
         
         tableView.registerClass(LoadCell.self, forCellReuseIdentifier: LoadCell.cellIdentifier())
         tableView.separatorStyle = .None
@@ -43,7 +93,7 @@ class TripPlanningViewController: UIViewController {
         tableView.showsVerticalScrollIndicator = false
         self.view.addSubview(tableView)
         constrain(tableView) { tableView in
-            tableView.top    == tableView.superview!.top + 134
+            tableView.top    == tableView.superview!.top + 160 + 21
             tableView.left   == tableView.superview!.left
             tableView.right  == tableView.superview!.right
             tableView.bottom == tableView.superview!.bottom
@@ -100,18 +150,25 @@ extension TripPlanningViewController : UICollectionViewDelegate, UICollectionVie
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(DateCell.cellIdentifier(), forIndexPath: indexPath) as! DateCell
         cell.dateLabel.text = String(indexPath.row)
-        if indexPath.row == 8 {
-            cell.setSelected(true)
+
+        if indexPath.row == 9 || indexPath.row == 2 {
+            cell.setUpdated(true)
         }
         else {
-            cell.setSelected(false)
+            cell.setUpdated(false)
         }
         return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(DateCell.cellIdentifier(), forIndexPath: indexPath) as! DateCell
+        cell.setSelected(true)
     }
 }
 
 class DateCell: UICollectionViewCell {
     let dateLabel = UILabel()
+    let dotView   = UIView()
     
     static func cellIdentifier() -> String {
         return "DateCell"
@@ -147,18 +204,42 @@ class DateCell: UICollectionViewCell {
             dateLabel.width  == 36
         }
         
+        dotView.backgroundColor = AppearanceManager.sharedInstance.azure
+        dotView.layer.borderColor = UIColor.clearColor().CGColor
+        dotView.layer.borderWidth = 1.0
+        dotView.layer.cornerRadius = 3.5
+        dotView.hidden = true
+        self.contentView.addSubview(dotView)
+        constrain(dotView) { dotView in
+            dotView.centerX == dotView.superview!.centerX
+            dotView.centerY == dotView.superview!.centerY + 20
+            dotView.height  == 7
+            dotView.width   == 7
+        }
+        
+    }
+    
+    private func setUpdated(updated: Bool) {
+        if updated {
+            dotView.hidden = false
+        }
+        else {
+            dotView.hidden = true
+        }
     }
     
     private func setSelected(selected: Bool) {
-        if selected {
-            dateLabel.font = AppearanceManager.regularFont(22)
-            dateLabel.textColor = .whiteColor()
-            dateLabel.backgroundColor = AppearanceManager.sharedInstance.azure
-        }
-        else {
-            dateLabel.font = AppearanceManager.extraLightFont(22)
-            dateLabel.textColor = AppearanceManager.sharedInstance.greyishBrownTwo
-            dateLabel.backgroundColor = .clearColor()
+        dispatch_async(dispatch_get_main_queue()) { 
+            if selected {
+                self.dateLabel.font = AppearanceManager.regularFont(22)
+                self.dateLabel.textColor = .whiteColor()
+                self.dateLabel.backgroundColor = AppearanceManager.sharedInstance.azure
+            }
+            else {
+                self.dateLabel.font = AppearanceManager.extraLightFont(22)
+                self.dateLabel.textColor = AppearanceManager.sharedInstance.greyishBrownTwo
+                self.dateLabel.backgroundColor = .clearColor()
+            }
         }
     }
 }
