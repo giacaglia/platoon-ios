86b7ffd2432fc075280a7e271a6c29a878dadd1b
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-31 17:04:07 -0400

    Bank information

--- platoon-ios/BankViewController.swift
+++ platoon-ios/BankViewController.swift
@@ -11,11 +11,15 @@ import Cartography
 
 class BankViewController : UIViewController {
     let tableView = UITableView()
+    
+    let titleQuestions = ["Business Name", "Federal Tax Indentification Number"]
+    let subtitleQuestions = ["Trucking Moving Inc.", "1237478123"]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = AppearanceManager.sharedInstance.backgroundColor
         self.title = "Bank Information"
         
+        self.addRightButton()
         tableView.registerClass(AnsweredCell.self, forCellReuseIdentifier: AnsweredCell.cellIdentifier())
         tableView.separatorStyle = .None
         tableView.delegate = self
@@ -30,16 +34,34 @@ class BankViewController : UIViewController {
             tableView.bottom == tableView.superview!.bottom
         }
     }
+    
+    private func addRightButton() {
+        let button: UIButton = UIButton(type: .Custom)
+        button.setTitle("Edit", forState: .Normal)
+        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button.frame = CGRectMake(0, 0, 35, 31)
+        button.addTarget(self, action: #selector(editBankInfo), forControlEvents: .TouchUpInside)
+        let barButton = UIBarButtonItem(customView: button)
+        self.navigationItem.rightBarButtonItem = barButton
+    }
+
+    func editBankInfo() {
+        
+    }
 }
 
 
 extension BankViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 2
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(AnsweredCell.cellIdentifier(), forIndexPath: indexPath) as! AnsweredCell
+        
+        cell.titleLabel.text = titleQuestions[indexPath.row]
+        cell.subtitleLabel.text = subtitleQuestions[indexPath.row]
+        cell.iconImageView.hidden = true
         return cell
     }
     
