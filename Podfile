41b7d38b0e084c1549e4fb917808e03b4ee6a25d
Giuliano Giacaglia <raulzito234@gmail.com>
2016-06-06 21:49:45 -0400

    Add firebase

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'platoon' do
+
+  # Pods for platoon
+  pod 'Firebase', '>= 2.5.1'
+
+
+  target 'platoon-iosTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'platoon-iosUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- platoon-ios/AppDelegate.swift
+++ platoon-ios/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
         splitViewController.delegate = self
+        FIRApp.configure()
         return true
     }
 
